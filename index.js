649ec4e08b65f14f242254e75ff9a1ed100d31b0
Your Name <you@example.com>
2024-03-28 00:46:40 +0500

    create simple calculator

---
+++ index.js
@@ -0,0 +1,36 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+    {
+        name: "FirstNumber",
+        type: "number",
+        message: "Enter your FirstNumber"
+    },
+    {
+        name: "SecondNumber",
+        type: "number",
+        message: "Enter your SecondNumber"
+    },
+    {
+        name: "Operation",
+        type: "list",
+        message: "Select one option to perform action",
+        choices: ["Addition", "Subtraction", "Multiplication", "Division"],
+    },
+]);
+// condition statement
+if (answer.Operation === "Addition") {
+    console.log(answer.FirstNumber + answer.SecondNumber);
+}
+else if (answer.Operation === "Subtraction") {
+    console.log(answer.FirstNumber - answer.SecondNumber);
+}
+else if (answer.Operation === "Multiplication") {
+    console.log(answer.FirstNumber * answer.SecondNumber);
+}
+else if (answer.Operation === "Division") {
+    console.log(answer.FirstNumber / answer.SecondNumber);
+}
+else {
+    console.log("Please select a valid number");
+}
